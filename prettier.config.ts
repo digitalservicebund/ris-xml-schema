7a60f8a5176a1661d7521de3cdb88604f4ad8026
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2026-03-30 16:30:37 +0200

    Update akn schema
    RISDEV-10833

--- prettier.config.ts
+++ prettier.config.ts
@@ -2,15 +2,7 @@ import { type Config } from "prettier";
 
 const config: Config = {
   plugins: ["@prettier/plugin-xml"],
-  xmlWhitespaceSensitivity: "preserve",
-  overrides: [
-    {
-      files: "*.xsd",
-      options: {
-        tabWidth: 4,
-      },
-    },
-  ],
+  xmlWhitespaceSensitivity: "ignore",
 };
 
 export default config;
