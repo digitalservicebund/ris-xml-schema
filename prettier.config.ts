a3904283a071b26890ce2b30e18a41f2e1297901
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2026-03-30 15:11:56 +0200

    Reduce whitespace sensitivity during formating
    RISDEV-10833

--- prettier.config.ts
+++ prettier.config.ts
@@ -2,6 +2,15 @@ import { type Config } from "prettier";
 
 const config: Config = {
   plugins: ["@prettier/plugin-xml"],
+  xmlWhitespaceSensitivity: "preserve",
+  overrides: [
+    {
+      files: "*.xsd",
+      options: {
+        tabWidth: 4,
+      },
+    },
+  ],
 };
 
 export default config;
