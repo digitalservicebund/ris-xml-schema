2713c8425481fb48590046f714869437b1c418c2
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2026-04-17 09:14:37 +0200

    Add gradle to build jar with schema files
    RISDEV-10833

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+import org.gradle.api.publish.maven.MavenPublication
+
+plugins {
+    base
+    `java-library`
+    `maven-publish`
+}
+
+group = "de.bund.digitalservice"
+version = "0.0.1"
+
+dependencies {}
+
+val packageSchemas = tasks.register<Jar>("packageSchemas") {
+    description = "Copies the schema files into the jar."
+    group = "build"
+
+    from("xsd") {
+        include("**/*.xsd")
+        into("schema")
+    }
+}
+
+tasks.assemble {
+    dependsOn(packageSchemas)
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    maven {
+        setUrl("https://plugins.gradle.org/m2/")
+    }
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "GitHubPackages"
+      url = uri("https://maven.pkg.github.com/digitalservicebund/ris-xml-schema")
+      credentials {
+        username = System.getenv("GITHUB_ACTOR")
+        password = System.getenv("GITHUB_TOKEN")
+      }
+    }
+  }
+  publications {
+    create<MavenPublication>("gpr") {
+      from(components["java"])
+    }
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ris-xml-schema"
