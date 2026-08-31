3d30a800da330b53569077ee1cca3b9cd53d8d20
Niklas Schröder <niklas.schroeder@digitalservice.bund.de>
2026-08-31 17:18:22 +0200

    Normalize output files
    All norm output files now have the `norm-` prefix. This makes it conform to the files in the `xsd/` directory of the other  e.g. caselaw, literature.
    
    Also removing the newly introduced `shared` naming and replace it with `norm-baukasten`. While `shared` might have been a bit more descriptive, `norm-baukasten` makes it immedeately visible, where it is based on: on `legalDocML.de-baukasten`.
    
    RISDEV-12090

--- scripts/patch-ldmlde.sh
+++ scripts/patch-ldmlde.sh
@@ -22,20 +22,20 @@ cd "$SCRIPTS_DIR"
 
 echo "Transform files"
 mkdir -p "$TEMP_OUTPUT_DIR"
-xsltproc "ldmlde/norm-baukasten.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-baukasten.xsd" > "$TEMP_OUTPUT_DIR/shared.xsd"
-xsltproc "ldmlde/norm-rechtsetzungsdokument.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-rechtsetzungsdokument.xsd" > "$TEMP_OUTPUT_DIR/rechtsetzungsdokument.xsd"
-xsltproc "ldmlde/norm-regelungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-regelungstext.xsd" > "$TEMP_OUTPUT_DIR/regelungstext.xsd"
-xsltproc "ldmlde/norm-offenestruktur.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-offenestruktur.xsd" > "$TEMP_OUTPUT_DIR/offenestruktur.xsd"
-xsltproc "ldmlde/norm-sonstigerveroeffentlichungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-sonstigerveroeffentlichungstext.xsd" > "$TEMP_OUTPUT_DIR/sonstigerveroeffentlichungstext.xsd"
+xsltproc "ldmlde/norm-baukasten.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-baukasten.xsd" > "$TEMP_OUTPUT_DIR/norm-baukasten.xsd"
+xsltproc "ldmlde/norm-rechtsetzungsdokument.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-rechtsetzungsdokument.xsd" > "$TEMP_OUTPUT_DIR/norm-rechtsetzungsdokument.xsd"
+xsltproc "ldmlde/norm-regelungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-regelungstext.xsd" > "$TEMP_OUTPUT_DIR/norm-regelungstext.xsd"
+xsltproc "ldmlde/norm-offenestruktur.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-offenestruktur.xsd" > "$TEMP_OUTPUT_DIR/norm-offenestruktur.xsd"
+xsltproc "ldmlde/norm-sonstigerveroeffentlichungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-sonstigerveroeffentlichungstext.xsd" > "$TEMP_OUTPUT_DIR/norm-sonstigerveroeffentlichungstext.xsd"
 
 mkdir -p "$TEMP_OUTPUT_DIR/metadata"
-xsltproc "ldmlde/norm-metadaten-rechtsetzungsdokument.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-rechtsetzungsdokument.xsd" > "$TEMP_OUTPUT_DIR/metadata/rechtsetzungsdokument.xsd"
-xsltproc "ldmlde/norm-metadaten-regelungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-regelungstext.xsd" > "$TEMP_OUTPUT_DIR/metadata/regelungstext.xsd"
-xsltproc "ldmlde/norm-metadaten-sonstiger-veroeffentlichungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-sonstiger-veroeffentlichungstext.xsd" > "$TEMP_OUTPUT_DIR/metadata/sonstiger-veroeffentlichungstext.xsd"
+xsltproc "ldmlde/norm-metadaten-rechtsetzungsdokument.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-rechtsetzungsdokument.xsd" > "$TEMP_OUTPUT_DIR/metadata/norm-rechtsetzungsdokument.xsd"
+xsltproc "ldmlde/norm-metadaten-regelungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-regelungstext.xsd" > "$TEMP_OUTPUT_DIR/metadata/norm-regelungstext.xsd"
+xsltproc "ldmlde/norm-metadaten-sonstiger-veroeffentlichungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-sonstiger-veroeffentlichungstext.xsd" > "$TEMP_OUTPUT_DIR/metadata/norm-sonstiger-veroeffentlichungstext.xsd"
 
 xsltproc --stringparam merge "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-frbr-metadaten-facetten-konsolidierte-fassung.sch" \
   "ldmlde/norm.sch.xsl"  \
-  "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de.sch" > "$TEMP_OUTPUT_DIR/shared.sch"
+  "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de.sch" > "$TEMP_OUTPUT_DIR/norm-shared.sch"
 
 echo "Copy transformed files to the schema directory…"
 mkdir -p "$ROOT_DIR/xsd/norm"
