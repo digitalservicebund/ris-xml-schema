aecabe36f71d8e5afc873a638fa11621f709eb4a
Niklas Schröder <niklas.schroeder@digitalservice.bund.de>
2026-08-31 17:25:11 +0200

    Remove metadata folder
    Just continuing to mirror the file structure of LDML.de. It allows us to keep the original namings from LDML.de by just always replacing the `LegalDocML.de` prefix with `norm`. This makes it easier to understand how the original files, the xsl files, and the output files relate - without having to look through the script.
    
    RISDEV-12090

--- scripts/patch-ldmlde.sh
+++ scripts/patch-ldmlde.sh
@@ -27,11 +27,9 @@ xsltproc "ldmlde/norm-rechtsetzungsdokument.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/l
 xsltproc "ldmlde/norm-regelungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-regelungstext.xsd" > "$TEMP_OUTPUT_DIR/norm-regelungstext.xsd"
 xsltproc "ldmlde/norm-offenestruktur.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-offenestruktur.xsd" > "$TEMP_OUTPUT_DIR/norm-offenestruktur.xsd"
 xsltproc "ldmlde/norm-sonstigerveroeffentlichungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-sonstigerveroeffentlichungstext.xsd" > "$TEMP_OUTPUT_DIR/norm-sonstigerveroeffentlichungstext.xsd"
-
-mkdir -p "$TEMP_OUTPUT_DIR/metadata"
-xsltproc "ldmlde/norm-metadaten-rechtsetzungsdokument.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-rechtsetzungsdokument.xsd" > "$TEMP_OUTPUT_DIR/metadata/norm-rechtsetzungsdokument.xsd"
-xsltproc "ldmlde/norm-metadaten-regelungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-regelungstext.xsd" > "$TEMP_OUTPUT_DIR/metadata/norm-regelungstext.xsd"
-xsltproc "ldmlde/norm-metadaten-sonstiger-veroeffentlichungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-sonstiger-veroeffentlichungstext.xsd" > "$TEMP_OUTPUT_DIR/metadata/norm-sonstiger-veroeffentlichungstext.xsd"
+xsltproc "ldmlde/norm-metadaten-rechtsetzungsdokument.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-rechtsetzungsdokument.xsd" > "$TEMP_OUTPUT_DIR/norm-metadaten-rechtsetzungsdokument.xsd"
+xsltproc "ldmlde/norm-metadaten-regelungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-regelungstext.xsd" > "$TEMP_OUTPUT_DIR/norm-metadaten-regelungstext.xsd"
+xsltproc "ldmlde/norm-metadaten-sonstiger-veroeffentlichungstext.xsl" "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-metadaten-sonstiger-veroeffentlichungstext.xsd" > "$TEMP_OUTPUT_DIR/norm-metadaten-sonstiger-veroeffentlichungstext.xsd"
 
 xsltproc --stringparam merge "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-frbr-metadaten-facetten-konsolidierte-fassung.sch" \
   "ldmlde/norm.sch.xsl"  \
@@ -39,8 +37,6 @@ xsltproc --stringparam merge "$TEMP_LDMLDE_DIR/Grammatiken/legalDocML.de-frbr-me
 
 echo "Copy transformed files to the schema directory…"
 mkdir -p "$ROOT_DIR/xsd/norm"
-mkdir -p "$ROOT_DIR/xsd/norm/metadata"
 rm -Rf "$ROOT_DIR/xsd/norm"/*.xsd
 rm -Rf "$ROOT_DIR/xsd/norm"/*.sch
-rm -Rf "$ROOT_DIR/xsd/norm/metadata"/*.xsd
 cp -R "$TEMP_OUTPUT_DIR"/* "$ROOT_DIR/xsd/norm/"
